5d09638fa2a255fb380cf7ab1054c0735907e7c1
PatricioOsorio <patricio.osorio@elektra.com.mx>
2025-12-06 18:25:45 -0600

    feat: config aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { HelloWorld } from '@presentation/components/HelloWorld';
 
 function App() {
   return (
     <>
       <h1>App</h1>
+      <HelloWorld />
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@domain': path.resolve(__dirname, './src/domain'),
+      '@application': path.resolve(__dirname, './src/application'),
+      '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
+      '@presentation': path.resolve(__dirname, './src/presentation'),
+    },
+  },
+});
